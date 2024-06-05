5c53f6dca0fce368f1d587f1f205bf859d28e9c6
Ismail Hossain <hyethereitsfahim@gmail.com>
2024-06-05 22:17:05 +0600

    type casting examples for integers, floats, and strings

---
+++ Foundation/type_castiing.py
@@ -0,0 +1,24 @@
+# --------------#
+# //* type-casting --> convert the data type of a value to another datatype.
+
+x = 69  # integer
+y = 6.9  # float
+z = "69"  # string
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+# Printing integer values
+print(f"Integer value of x: {int(x)}")
+print(f"Integer value of y: {int(y)}")
+print(f"Integer value of z: {int(z)}")
+
+# Printing float values 
+print(f"Float value of x: {float(x)}")
+print(f"Float value of y: {float(y)}")
+print(f"Float value of z: {float(z)}")
+
+print(f"String value for x: {str(x)}")
+print(f"String value for y: {str(y)}")
+print(f"This is already into string :{z}")
