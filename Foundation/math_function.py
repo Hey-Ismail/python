ea8509443fd9305c580dcea611af377e52975aa7
Ismail Hossain <hyethereitsfahim@gmail.com>
2024-06-05 22:18:33 +0600

    basic math operations in python

---
+++ Foundation/math_function.py
@@ -0,0 +1,11 @@
+import math
+
+print(round(3.1416))
+print(math.pi)
+print(round(math.sqrt(9)))
+print(round(math.pow(5, 2)))
+print(abs(-69))
+print(math.floor(6.9))
+print(math.ceil(6.9))
+print(max(6, 0, 9))
+print(min(6, 0, 9))
