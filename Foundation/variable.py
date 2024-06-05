6ffb857696145cfe892a2bb1ca061b1ac5802c74
Ismail Hossain <hyethereitsfahim@gmail.com>
2024-06-05 22:15:10 +0600

    implementing variable examples and multiple assignment

---
+++ Foundation/variable.py
@@ -0,0 +1,24 @@
+# -----------#
+# //* Variable -- A variable acts like a labeled container that stores data within computer's memory.
+
+int = 69
+floatt = 6.9
+string = "Ismail Hossain fahim"
+boolean = True
+print(type(int))
+print(type(floatt))
+print(type(string))
+print(type(boolean))
+print("This is an integer : " + str(int))
+print("This is an float :" + str(floatt))
+print("This is a string " + string)
+print("This is a boolean value :" + str(boolean))
+
+# //* Multiple Assignment --Its allows us to assign multiple variables at the same time in one line.
+
+name, age, is_married = "Ismail Hossain Fahim", 21, False
+print(f"This person's name is {name}, He is {age} years old and he is {is_married}")
+Ismail = Hossain = Fahim = 21
+print(f"Ismail  is {Ismail} years old")
+print(f"Hossain is {Hossain} years old")
+print(f"Fahim   is {Fahim} years old")
