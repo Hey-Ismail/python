3f954d779cb43f6399fa445f5b898d771a929986
Ismail Hossain <hyethereitsfahim@gmail.com>
2024-06-05 22:17:37 +0600

    user input functionality and display information

---
+++ Foundation/user_input.py
@@ -0,0 +1,15 @@
+# -----------------#
+# //* input function --> where i can take user information.
+
+name = input("What's your name : ")
+age = int(input("How old are you : "))
+academic = input("Which school/collage/university are you currently :")
+hobby = input("Tell us about your hobby : ")
+dream = input("What is your dream : ")
+
+# Answer
+print(f"assalamu alaikum , {name}")
+print(f"you were born in {2024-age}")
+print(f"Currently you are in {academic}")
+print(f"Your hobby  is {hobby}")
+print(f"Your dream job is {dream}")
